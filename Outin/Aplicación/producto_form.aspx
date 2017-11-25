d987ba077d6bb3643138eec6f723bd8c82f84a0b
luoliver <lfmonroy67@misena.edu.co>
2017-11-24 19:37:35 -0500

    formulario de ingresar producto

---
+++ Outin/Aplicación/producto_form.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterOutIn.Master" AutoEventWireup="true" CodeBehind="producto_form.aspx.cs" Inherits="Aplicación.producto_form" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+
+
+    <div>
+    <asp:Label ID="lbl_Titulo" runat="server" Text="Insertar Producto"></asp:Label>
+     </div>
+    <asp:label id="lbl_nomProducto" runat="server" text="Nombre" for="txt_nomProducto"></asp:label>
+    <asp:TextBox ID="txt_nomProducto" runat="server"></asp:TextBox>
+    <asp:label id="lbl_marcaProducto" runat="server" text="Marca" for="txt_marcaProducto"></asp:label>
+    <asp:TextBox ID="txt_marcaProducto" runat="server"></asp:TextBox>
+    <asp:label id="lbl_stockMin" runat="server" text="Stock Minimo" for="txt_stockMin"></asp:label>
+    <asp:TextBox ID="txt_stockMin" runat="server"></asp:TextBox>
+    <asp:label id="lbl_stockMax" runat="server" text="Stock Maximo" for="txt_stockMax"></asp:label>
+    <asp:TextBox ID="txt_stockMax" runat="server"></asp:TextBox>
+    <asp:label id="lbl_peso" runat="server" text="Peso" for="txt_peso"></asp:label>
+    <asp:TextBox ID="txt_peso" runat="server"></asp:TextBox>
+    <asp:label id="lbl_estado" runat="server" text="Estado" for="txt_estado"></asp:label>
+    <asp:TextBox ID="txt_estado" runat="server"></asp:TextBox>
+    <asp:label id="lbl_categoria" runat="server" text="Categoria" for="txt_categoria"></asp:label>
+    <asp:TextBox ID="txt_categoria" runat="server"></asp:TextBox>
+    <asp:button runat="server" text="Registrar Producto" id="btn_enviarProducto" />
+    
+
+</asp:Content>
---
+++ Outin/Aplicación/producto_form.aspx.designer.cs
@@ -0,0 +1,159 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Aplicación {
+    
+    
+    public partial class producto_form {
+        
+        /// <summary>
+        /// Control lbl_Titulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_Titulo;
+        
+        /// <summary>
+        /// Control lbl_nomProducto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_nomProducto;
+        
+        /// <summary>
+        /// Control txt_nomProducto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_nomProducto;
+        
+        /// <summary>
+        /// Control lbl_marcaProducto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_marcaProducto;
+        
+        /// <summary>
+        /// Control txt_marcaProducto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_marcaProducto;
+        
+        /// <summary>
+        /// Control lbl_stockMin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_stockMin;
+        
+        /// <summary>
+        /// Control txt_stockMin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_stockMin;
+        
+        /// <summary>
+        /// Control lbl_stockMax.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_stockMax;
+        
+        /// <summary>
+        /// Control txt_stockMax.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_stockMax;
+        
+        /// <summary>
+        /// Control lbl_peso.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_peso;
+        
+        /// <summary>
+        /// Control txt_peso.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_peso;
+        
+        /// <summary>
+        /// Control lbl_estado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_estado;
+        
+        /// <summary>
+        /// Control txt_estado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_estado;
+        
+        /// <summary>
+        /// Control lbl_categoria.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_categoria;
+        
+        /// <summary>
+        /// Control txt_categoria.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_categoria;
+        
+        /// <summary>
+        /// Control btn_enviarProducto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_enviarProducto;
+    }
+}
