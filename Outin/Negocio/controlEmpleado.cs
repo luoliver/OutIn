c7866a9ee0792b0f6cdb3444406e81ef6be6d7b3
luoliver <lfmonroy67@misena.edu.co>
2017-11-27 09:58:50 -0500

    modelo usuaro

---
+++ Outin/Negocio/controlEmpleado.cs
@@ -0,0 +1,32 @@
+﻿using DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class controlEmpleado:Respuesta
+    {
+        OutInDBDataContext data = new OutInDBDataContext();
+
+        public bool insertarEmpleado(string nacimiento,string nombre, string tipoID, string direccion, string cargo, string contacto, string fecha_entrada, string estado)
+        {
+            try
+            {
+                data.insertarEmpleado(Convert.ToDateTime(nacimiento), nombre, tipoID, direccion, cargo, contacto, Convert.ToDateTime(fecha_entrada), estado);
+                Codigo = "OK";
+                Rta = "Se ingreso el empleado correctamente";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Codigo = "Error";
+                Rta = ex.Message;
+                return false;
+            }
+
+        }
+    }
+}
