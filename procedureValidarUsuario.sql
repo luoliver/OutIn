715f013048768dfecf6c8bf9849533e134012338
luoliver <33701369+luoliver@users.noreply.github.com>
2017-12-06 13:27:19 -0500

    Delete procedureValidarUsuario.sql

--- procedureValidarUsuario.sql
+++
@@ -1,7 +0,0 @@
-create procedure validarUsuario
-(@usuario varchar(30),@contrasena varchar(50))
-as 
-begin
-select * from Empleado 
-where em_identificacion = @usuario and em_contrasena = @contrasena
-end
